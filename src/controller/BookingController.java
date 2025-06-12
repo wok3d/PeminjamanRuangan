e0f9b8bc907ee3be8f9b756b47be84bef68caa6f
Dzaki Azharraya <temempicc7@gmail.com>
2025-06-13 00:15:56 +0700

    Remove unused files and refactor directory structure

--- src/controller/BookingController.java
+++
@@ -1,9 +0,0 @@
-package controller;
-
-import model.Database;
-
-public class BookingController {
-    public static boolean isSlotBooked(String room, String date, String slot) {
-        return Database.checkBooking(room, date, slot);
-    }
-}
